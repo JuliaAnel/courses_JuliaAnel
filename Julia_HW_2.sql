f1a70b5c5c344a7ebec4d1a1e2ecd3a659510ab8
JuliaAnel <77753687+JuliaAnel@users.noreply.github.com>
2021-10-29 19:39:57 +0300

    Update Julia_HW_2.sql

--- Julia_HW_2.sql
+++ Julia_HW_2.sql
@@ -1,8 +1,4 @@
 
---Создать таблицу employees
--- - id. serial,  primary key,
--- - employee_name. Varchar(50), not null
--- Наполнить таблицу employee 70 строками
 
 create table employees (
 id serial primary key, 
@@ -12,7 +8,7 @@ employee_name varchar (50) not null
 insert into employees (id, employee_name)
 values (default, 'Julia');
 
-
+--=====================================
 
 create table salary (
 id serial primary key,
@@ -67,11 +63,7 @@ values (default, 2400);
 insert into salary (id, monthly_salary)
 values (default, 2500);
 
-select * from salary;
---Создать таблицу employee_salary
--- - id. Serial  primary key,
--- - employee_id. Int, not null, unique
--- - salary_id. Int, not null
+--=============================
 
 create table employee_salary (
 id serial primary key,
@@ -83,11 +75,7 @@ insert into employee_salary (id, employee_id, salary_id)
 values (default, 99, 16
 );
 
---Создать таблицу roles
--- - id. Serial  primary key,
--- - role_name. int, not null, unique
--- Поменять тип столба role_name с int на varchar(30)
--- Наполнить таблицу salary 20 строками:
+--=============================
 
 create table roles (
 id serial primary key, 
@@ -100,11 +88,7 @@ using role_name::varchar(30);
 insert into roles (id, role_name)
 values (default, 'Senior Automation QA engineer');
 
---Создать таблицу roles_employee
--- - id. Serial  primary key,
--- - employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
--- - role_id. Int, not null (внешний ключ для таблицы roles, поле id)
--- Наполнить таблицу salary 40 строками:
+--==============================
 
 create table roles_employee (
 id serial primary key, 
@@ -120,4 +104,3 @@ insert into roles_employee (id, employee_id, role_id)
 values (default, 56, 14);
 
 
-select * from roles_employee;
